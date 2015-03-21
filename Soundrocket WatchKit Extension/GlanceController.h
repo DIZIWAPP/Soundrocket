a9478b3f0d40b77a1425799842faf20e1e6ea97c
Sebastian Boldt <sebastian.boldt.1989@googlemail.com>
2015-03-21 21:12:16 +0100

    added watchkit boiler-plate code

---
+++ Soundrocket WatchKit Extension/GlanceController.h
@@ -0,0 +1,14 @@
+//
+//  GlanceController.h
+//  Soundrocket WatchKit Extension
+//
+//  Created by Sebastian Boldt on 21.03.15.
+//  Copyright (c) 2015 sebastianboldt. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface GlanceController : WKInterfaceController
+
+@end
